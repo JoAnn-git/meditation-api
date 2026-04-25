6613311a7c5596d98b9c69abcade913b8f356868
JoAnn <zhuann.w@gmail.com>
2026-04-25 13:09:50 +1200

    add welcom page

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🧘 Meditation API</h1>
+    <p>Welcome! This is a backend API project.</p>
+    <p>Available endpoints:</p>
+    <ul>
+      <li>GET /sessions</li>
+      <li>POST /sessions</li>
+      <li>GET /sessions/:id</li>
+      <li>DELETE /sessions/:id</li>
+    </ul>
+    <p>you will see more soon, thank for my friends support(spiritual support)</p>
+  `);
+});
